849adc77fb5a1f2fb9979277dded0c777345b77a
Daniel Samko <gekk0n.ua@gmail.com>
2022-05-23 21:20:41 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+if (!path.join(__dirname, 'files-copy')){
+  fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {if (err) throw err;});
+}
+
+fs.readdir(path.join(__dirname, 'files'), 'utf-8', (err, file) => {
+  if (err) throw err;
+  file.forEach( (el) => {
+    fs.copyFile(path.join(__dirname, `files\\${el}`), path.join(__dirname, `files-copy\\${el}`), (err) => {
+      if (err) throw err;
+    });
+  });
+});
