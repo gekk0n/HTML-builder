8b18e249dacfb0a5bee0c9dd72b22d9d844d77a0
Daniel Samko <gekk0n.ua@gmail.com>
2022-05-26 19:49:02 +0300

    fix: add correct isFile() validation in task 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const fs = require('fs');
+const secretPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(path.join(__dirname, 'secret-folder'),
+fs.readdir(secretPath,
   'utf-8',
   (err, data) => {
     if (err) throw err;
     data.forEach(file => {
-      fs.stat('03-files-in-folder\\secret-folder\\' +file, 
+      fs.stat(path.join(secretPath, file),
         (err, stat) => {
           if (err) throw err;
-          if (stat.size !== 0) {
+          if (stat.isFile()) {
             let name = file.split('.').join(' ');
             console.log(`${name} ${stat.size / 1000} Kb`);
           }
