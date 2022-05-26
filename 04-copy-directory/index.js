e250a7479a478114aa7f614f026efa6fcf366191
Daniel Samko <gekk0n.ua@gmail.com>
2022-05-26 19:56:39 +0300

    fix: fix path in copy file in task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,12 +3,16 @@ const path = require('path');
 const copyPath = path.join(__dirname, 'files-copy');
 const filesPath = path.join(__dirname, 'files');
 
-fs.mkdir(copyPath, (err) => {if (err) throw err;});
+fs.mkdir(copyPath,
+  {
+    recursive: true,
+  },
+  (err) => {if (err) throw err;});
 
 fs.readdir(filesPath, 'utf-8', (err, file) => {
   if (err) throw err;
   file.forEach( (el) => {
-    fs.copyFile(path.join(__dirname, `files\\${el}`), path.join(__dirname, `files-copy\\${el}`), (err) => {
+    fs.copyFile(path.join(filesPath, el), path.join(copyPath, el), (err) => {
       if (err) throw err;
     });
   });
