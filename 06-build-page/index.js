570f3ce75adbb2473b3bc497149491a31d75d255
Daniel Samko <gekk0n.ua@gmail.com>
2022-11-08 01:04:19 +0300

    feat: 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,11 +1,85 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
-const projectDir = path.join(__dirname, 'project-dist');
+const projectFolder = path.join(__dirname, 'project-dist');
+const templatefile = path.join(__dirname, 'template.html');
+const assetsFolder = path.join(__dirname, 'assets');
+const distAssetsFolder = path.join(__dirname, 'project-dist', 'assets');
+const stylesFolder = path.join(__dirname, 'styles');
+const componentsFolder = path.join(__dirname, 'components');
+const re = /{{.{0,}}}/gm;
 
-fs.mkdir(projectDir, (err) => {
-  if (err) throw err;
-  fs.copyFile('06-build-page\\template.html', `${projectDir}\\template.html`, (err) => {
+
+
+async function createFolders () {
+  await fsPromises.mkdir(projectFolder, { recursive: true});
+  fs.cp(assetsFolder, distAssetsFolder, {recursive: true}, (err) => {
     if (err) throw err;
-    fs.rename(`${projectDir}\\template.html`, `${projectDir}\\index.html`, () =>{});
   });
-});
+}
+
+
+async function createHtml () {
+  await fsPromises.copyFile(templatefile, path.join(projectFolder, 'template.html'));
+  await fsPromises.rename(path.join(projectFolder, 'template.html'), path.join(projectFolder, 'index.html'));
+  fs.readFile(path.join(projectFolder, 'index.html'), 'utf8', (err, data) => {
+    if (err) throw err;
+    let indexContent = data.toString();
+
+
+    let templateTags = indexContent.match(re);
+    
+
+    templateTags.forEach(tag => {
+      let currentTag = tag.replace(/[^a-z]/gi, '');
+      fs.readFile(path.join(componentsFolder, `${currentTag}.html`), {withFileTypes: true}, (err, data) => {
+        if (err) throw err;
+        let content = data.toString();
+        indexContent = indexContent.replace(`{{${currentTag}}}`, content);
+        fs.writeFile(path.join(projectFolder, 'index.html'), indexContent, (err) => {
+          if (err) throw err;
+        });
+      });
+    });
+  });
+  
+  // console.log(resultPage);
+  
+}
+// console.log(path.parse(path.join(projectFolder, 'index.html')));
+
+
+
+async function copyStyles () {
+  fs.writeFile(path.join(projectFolder, 'style.css'), '', (err) => {
+    if (err) throw err;
+  });
+  fs.readdir(stylesFolder, {
+    withFileTypes: true
+  },
+  (err, file) => {
+    if (err) throw err;
+    file.forEach(el => {
+      if (el.isFile() && path.extname(`${stylesFolder}\\${el.name}`) === '.css') {
+        fs.readFile(`${stylesFolder}\\${el.name}`, 'utf-8', (err, data) => {
+          if (err) throw err;
+          fs.appendFile(path.join(projectFolder, 'style.css'), data, () => {});
+        });
+      }
+    });
+  });
+}
+
+
+
+async function buildPage() {
+  await createFolders();
+  await createHtml();
+  await copyStyles();
+}
+
+buildPage();
+
+
+
+
