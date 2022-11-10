3ff197bcca07b41f31c2040d84aa60ffeeab8b11
Daniel Samko <gekk0n.ua@gmail.com>
2022-11-10 19:08:43 +0300

    fix: fix paths

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const stylesPath = path.join(__dirname, 'styles');
-const bundlePath = path.join(__dirname, 'project-dist\\bundle.css');
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
 fs.writeFile(bundlePath, '', (err) => {
   if (err) throw err;
@@ -10,8 +10,8 @@ fs.readdir(stylesPath, { withFileTypes: true },
   (err, file) => {
     if (err) throw err;
     file.forEach(el => {
-      if (el.isFile() && path.extname(`${stylesPath}\\${el.name}`) === '.css') {
-        fs.readFile(`${stylesPath}\\${el.name}`, 'utf-8', (err, data) => {
+      if (el.isFile() && path.extname(path.join(stylesPath, `${el.name}`)) === '.css') {
+        fs.readFile(path.join(stylesPath, `${el.name}`), 'utf-8', (err, data) => {
           if (err) throw err;
           fs.appendFile(bundlePath, data, () =>{});
         });
