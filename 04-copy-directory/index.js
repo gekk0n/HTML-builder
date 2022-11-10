8d7dda87b8de9a9a2dea36cc0e0ab2109a6ddc28
Daniel Samko <gekk0n.ua@gmail.com>
2022-11-10 17:49:15 +0300

    fix: fix 04-copy-directory function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,27 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 const copyPath = path.join(__dirname, 'files-copy');
 const filesPath = path.join(__dirname, 'files');
 
-fs.mkdir(copyPath,
-  {
-    recursive: true,
-  },
-  (err) => {if (err) throw err;});
-
-fs.readdir(filesPath, 'utf-8', (err, file) => {
-  if (err) throw err;
-  file.forEach( (el) => {
-    fs.copyFile(path.join(filesPath, el), path.join(copyPath, el), (err) => {
+async function copyDirectory () {
+  await fsPromises.mkdir(copyPath, {recursive: true});
+  const files = await fsPromises.readdir(copyPath);
+  files.forEach((file) => {
+    fs.rm(path.join(__dirname, 'files-copy', `${file}`), (err) => {
       if (err) throw err;
     });
+  }); 
+  await fsPromises.access(filesPath);
+  fs.readdir(filesPath, 'utf-8', (err, file) => {
+    if (err) throw err;
+    file.forEach( (el) => {
+      fs.copyFile(path.join(filesPath, el), path.join(copyPath, el), (err) => {
+        if (err) throw err;
+      });
+    });
   });
-});
+}
+
+
+copyDirectory ();
